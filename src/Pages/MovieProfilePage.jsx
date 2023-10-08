5e7918d6158fd7525c7a6cb51aad7714aa9aaa71
Alex Chachkhiani <devfterr@gmail.com>
2023-10-08 11:47:44 +0400

    made the page with react routing

--- src/Pages/MovieProfilePage.jsx
+++ src/Pages/MovieProfilePage.jsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom"
+
 export default function MovieProfilePage() {
+  const params = useParams()
+  console.log(params)
   return (
-    <h1>Hello, this is from MovieProfilePage</h1>
+    <h1>Hello, the name of the film is {params.moviesName}</h1>
   )
 }
--- src/Pages/MoviesPage.jsx
+++ src/Pages/MoviesPage.jsx
@@ -1,5 +1,34 @@
+import { useNavigate } from "react-router-dom";
+
 export default function MoviesPage() {
+  const data = [
+    {
+      description: "Lord of the rings",
+      id: 0,
+    },
+    {
+      description: "Harry Potter",
+      id: 1,
+    },
+    {
+      description: "God father",
+      id: 2,
+    },
+    {
+      description: "Sweet november",
+      id: 3,
+    },
+  ];
+  const navigate = useNavigate()
   return (
-    <h1>Hello, this is from MoviesPage</h1>
-  )
+    <ul>
+      {data.map((item) => {
+        return (
+          <li style={{ cursor: "pointer", marginLeft: "40px" }} key={item.id} onClick={() => navigate(`${item.description}`)}>
+            {item.description}
+          </li>
+        )
+      })}
+    </ul>
+  );
 }
